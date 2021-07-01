b87d3cb574ab8ef9a2a3c8399497cbd05a87dbbc
Mark Sujew <mark.sujew@typefox.io>
2021-07-01 14:42:23 +0000

    Removed copyfiles dev dependency

--- packages/langium-cli/src/generator/grammar-serializer.ts
+++ packages/langium-cli/src/generator/grammar-serializer.ts
@@ -1,3 +1,9 @@
+/******************************************************************************
+ * Copyright 2021 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
 import { CompositeGeneratorNode, Grammar, LangiumServices, NL, process } from 'langium';
 import { LangiumConfig } from '../package';
 import { generatedHeader } from './util';
@@ -9,11 +15,15 @@ export function serializeGrammar(services: LangiumServices, grammar: Grammar, co
 
     if (config.langiumInternal) {
         node.children.push("import { loadGrammar } from '../grammar-util';", NL);
-        node.children.push("import { Grammar } from './ast';", NL, NL);
+        node.children.push("import { Grammar } from './ast';");
     } else {
-        node.children.push("import { loadGrammar, Grammar } from 'langium';", NL, NL);
+        node.children.push("import { loadGrammar, Grammar } from 'langium';");
     }
+    node.children.push(NL, NL);
 
-    node.children.push('const grammar = (): Grammar => loadGrammar(`', json.replace(/\\/g, '\\\\'), '`);', NL, NL, 'export default grammar;');
+    node.children.push(
+        'const grammar = (): Grammar => loadGrammar(`', json.replace(/\\/g, '\\\\'), '`);', NL, NL,
+        'export default grammar;', NL
+    );
     return process(node);
 }
